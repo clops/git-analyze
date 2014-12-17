5e725ffc594c01d30d9f7bfd59d172233ba78419
Matthias Beyer <mail@beyermatthias.de>
2014-12-17 19:55:31 +0100

    Add script to generate todos-per-merge

---
+++ todos-per-merge.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+OLD_HEAD=$(git log HEAD -n 1 --format=format:%H)
+todostr="@todo"
+
+tmpfile=/tmp/$$-gnuplot
+
+for merge in $(git log --merges --format=format:"%H" --reverse)
+do
+    git checkout "$merge" >/dev/null
+    dat=$(git log --format=format:"%ci" HEAD -n 1 | sed 's/\ +[0-9]*//' | \
+            sed 's/\ /T/')
+    nto=$(git grep $todostr | wc -l)
+
+    echo "'$dat' $nto"
+done > $tmpfile
+
+gnuplot -e "
+set output 'todos-per-merge.png';
+set term png truecolor giant;
+set xdata time;
+set timefmt  \"'%Y-%m-%dT%H:%M:%S'\";
+set format x \"'%Y-%m-%dT%H:%M:%S'\";
+plot '$tmpfile' using 1:2 with lines notitle;
+"
+
+git checkout $OLD_HEAD
